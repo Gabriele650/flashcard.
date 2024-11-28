422ecedbd95e83be7e5cf193186b235a3c612fe4
Gabriele650 <00001095353974SP@al.educacao.sp.gov.br>
2024-11-28 11:44:33 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -15,5 +15,18 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
-
-
+criaCartao(
+    'História',
+    'Quando foi a primeira revolução industrial?',
+    '1760'
+)
+criaCartao(
+    'Geografia',
+    'Quando aconteceu a segunda guerra mundial?',
+    '1 de setembro de 1939'
+)
+criaCartao(
+    'História',
+    'Quando foi a revolução francesa?',
+    'Entre 1789 e 1799'
+)
